49ba2becdc4a13bb543b957c83a307a884939b75
BOOPATHI RAJA <boopathirajaboobi@gmail.com>
2024-06-24 22:48:55 +0530

    24-06-2024

---
+++ basic.js
@@ -0,0 +1,9 @@
+/*let arr = [10, 20, 30, 40, 50, 60, 80, 90];
+document.write(arr.splice(1, 3, 20, 30, 40) + "     ");
+
+document.write(arr);
+let animals = ["dog", "cat", "horse", "cow"];
+for (let i in animals) {
+  document.getElementsByTagName("p")[i].innerHTML = animals;
+}
+*/
---
+++ day4.js
@@ -0,0 +1,41 @@
+/*let name = {
+  penname: "stylish",
+  pencolor: "blue",
+  penprice: "7rps",
+};
+console.log(
+  "the pen i bought yesterday is " +
+    name.penname +
+    " for " +
+    name.penprice +
+    " that is in " +
+    name.pencolor +
+    " color"
+);
+let name1 = "Akki";
+let name2 = "diwa";
+name1 === name2 ? console.log("yes") : console.log("no");
+
+let diwan = prompt("guess the number between 1 to 10");
+let crt = 6;
+let n = 1;
+let flag = 0;
+h = 4;
+
+while (n < 5) {
+  if (diwan != crt) {
+    window.alert("oops wrong ans you have another " + h + " chance");
+    diwan = window.prompt("try again");
+    h--;
+
+    n++;
+  } else {
+    flag = 1;
+    break;
+  }
+}
+if (flag === 1) {
+  window.alert("Congrats you are right!");
+} else {
+  window.alert("Sorry you lost");
+}*/
